5ebb6a0017ffb1584be48425a9a2daa2ca42be7a
prakhar21 <pmprakhargenius@gmail.com>
2020-01-17 13:39:02 +0530

    adding attention viz

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import torch
+import numpy as np
 from pytorch_transformers import BertTokenizer, BertForMaskedLM
 import nltk
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+model = BertForMaskedLM.from_pretrained('bert-base-uncased', output_attentions=True)
 model.eval()
 
 @app.route('/fillblanks', methods=['POST'])
@@ -39,10 +40,26 @@ def predict():
 	with torch.no_grad():
 	    outputs = model(tokens_tensor, token_type_ids=segments_tensors)
 	    predictions = outputs[0]
+	    attention = outputs[-1]
 
+	dim = attention[2][0].shape[-1]*attention[2][0].shape[-1]
+	a = attention[2][0].reshape(12, dim)
+	b = a.mean(axis=0)
+	c = b.reshape(attention[2][0].shape[-1],attention[2][0].shape[-1])
+	avg_wgts = c[masked_index]
+	#print (avg_wgts, tokenized_text)
+	focus = [tokenized_text[i] for i in avg_wgts.argsort().tolist()[::-1] if tokenized_text[i] not in ['[SEP]', '[CLS]', '[MASK]']][:5]
+
+	# for layer in range(12):
+	# 	weights_layer = np.array(attention[0][0][layer][masked_index])
+	# 	print (weights_layer, tokenized_text)
+	# 	print (weights_layer.argsort()[-3:][::-1])
+	# 	print ()
 	predicted_index = torch.argmax(predictions[0, masked_index]).item()
 	predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
-	return sentence_orig.replace('____', '<font color="red"><b>'+predicted_token+'</b></font>')
+	for f in focus:
+		sentence_orig = sentence_orig.replace(f, '<font color="blue">'+f+'</font>')
+	return sentence_orig.replace('____', '<font color="red"><b><i>'+predicted_token+'</i></b></font>')
 
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run(debug=False)
